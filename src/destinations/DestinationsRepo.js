6240631ce0ef58cccf486f8c7e0380a0f54eb75f
Hannah.Celemen <hcelemen@icloud.com>
2022-09-26 20:56:19 -0600

    Refactor destination classes and tests

--- src/destinations/DestinationsRepo.js
+++ src/destinations/DestinationsRepo.js
@@ -6,6 +6,11 @@ class DestinationsRepo {
   getDestinationById(destinationId) {
     return this.destinationsData.find(destination => destination.id === destinationId);
   }
+
+  getDestinationIdByName(locationName) {
+    const destination = this.destinationsData.find(obj => obj.destination === locationName)
+    return destination.id;
+  }
 };
 
 export default DestinationsRepo;
